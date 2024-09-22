d59e52af41dc90acdb48e30998286cee47136ba5
Samirsrz <shahilahamed2001@gmail.com>
2024-09-23 02:18:42 +0600

    added json webtoken and cors policy

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const jwt = require('jsonwebtoken')
 const port = process.env.port || 8000
 
 // middleware
@@ -51,10 +53,35 @@ async function run() {
   try {
     
    // await client.connect();
-  
+  app.post('/jwt', async(req, res) => {
+      const user= req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn : '365d',
+      })
+      res 
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({ success: true })
+  })
    
 
-
+  //Logout 
+app.get('/logout', async (req,res) => {
+   try{
+    res.clearCookie('token', {
+      maxAge:0,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    })
+    .send({ success: true })
+    console.log('Logout successful')
+   }catch (err) {
+    res.status(500).send(err)
+  }
+})
 
 
 
