e8c6ea571b08d90503ea394a52ab9ec15abf362b
shaishabcoding <myshaishab@gmail.com>
2024-09-23 07:39:38 +0600

    added get jobs

--- index.js
+++ index.js
@@ -1,104 +1,121 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-require('dotenv').config()
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const jwt = require('jsonwebtoken')
-const port = process.env.port || 8000
+const jwt = require("jsonwebtoken");
+const port = process.env.port || 8000;
 
 // middleware
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
-    credentials: true,
-    optionSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions))
-  
-  app.use(express.json())
-  app.use(cookieParser())
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
- const verifyToken = async(req, res, next) => {
-   const token = req.cookies?.token
-   console.log(token);
-   if(!token){
-    return res.status(401).send({message: "unauthorized access"})
+app.use(express.json());
+app.use(cookieParser());
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-     if(err){
-      console.log(err)
-      return res.status(401).send({message: 'unauthorized access'})
-     }
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: "unauthorized access" });
+    }
 
-     req.user = decoded;
-    next()
-  })
-}
-
-
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2aarqjz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
-   // await client.connect();
-  app.post('/jwt', async(req, res) => {
-      const user= req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn : '365d',
-      })
-      res 
-      .cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-      })
-      .send({ success: true })
-  })
-   
+    // await client.connect();
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
-  //Logout 
-app.get('/logout', async (req,res) => {
-   try{
-    res.clearCookie('token', {
-      maxAge:0,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    })
-    .send({ success: true })
-    console.log('Logout successful')
-   }catch (err) {
-    res.status(500).send(err)
-  }
-})
+    //Logout
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+        console.log("Logout successful");
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
 
+    const jobCollection = db.collection("jobs");
 
-
-
-
+    //  jobs related api
+    app.get("/jobs", async (req, res) => {
+      try {
+        const results = await jobCollection.find().toArray();
+        res.status(200).send({
+          success: true,
+          message: "Job get successfully",
+          data: results,
+        });
+      } catch (error) {
+        res.status(400).send({
+          success: false,
+          message: "Something went wrong",
+          data: error,
+        });
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello from jobHunting..')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from jobHunting..");
+});
 
 app.listen(port, () => {
-  console.log(`Job Hunting is running on port ${port}`)
-})
+  console.log(`Job Hunting is running on port ${port}`);
+});
