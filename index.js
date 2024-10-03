5540213358bf84f98435b88e813fd4f0215ad7f5
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-10-04 00:50:45 +0600

    featured

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    
+
     "https://job-hunting-job-seekers.vercel.app"
   ],
   credentials: true,
@@ -53,17 +53,18 @@ const client = new MongoClient(uri, {
 async function run() {
 
   try {
-    
+
     const db = client.db("job-hunting");
     const jobCollection = db.collection("jobs");
     const appliesCollection = db.collection("applies");
     const companyJobsCollection = db.collection("jobs");
-    const featuredcompanyJobsCollection = db.collection("jobs");
+
     const usersCollection = db.collection("users");
     const companyCollection = db.collection("companies");
 
     // // followers collection
-
+    // // featured collection
+    const featuredcompanyJobsCollection = db.collection("jobs");
     const followersCollection = db.collection('followers')
 
     // await client.connect();
@@ -89,36 +90,36 @@ async function run() {
       }
     });
 
-   //user saving in DB route 
+    //user saving in DB route 
 
-  app.put('/user', async(req, res) => {
-     const user = req.body;
-     const query = {email : user?.email}
+    app.put('/user', async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email }
 
-     const isExist  = await usersCollection.findOne(query);
-     if(isExist){
-      return res.send(isExist)
-     }
-     const options = {upsert : true}
-     const updateDoc = {
-      $set: {
-        ...user,
-        timestamp: Date.now()
-      },
-     }
-     const result = await usersCollection.updateOne(query, updateDoc, options)
-     res.send(result);
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send(isExist)
+      }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now()
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      res.send(result);
 
-  })
-  
+    })
 
-  //saving company data into Db
-  app.post('/company-data', async(req, res) => {
-     const query = req.body;
-     const result = await companyCollection.insertOne(query);
-     res.send(result);
 
-  })
+    //saving company data into Db
+    app.post('/company-data', async (req, res) => {
+      const query = req.body;
+      const result = await companyCollection.insertOne(query);
+      res.send(result);
+
+    })
 
 
 
@@ -312,7 +313,7 @@ async function run() {
           if (posted.acknowledged == true) {
             let result = await featuredcompanyJobsCollection.find().toArray()
             console.log(result);
-            
+
             res.send(result)
           }
 
@@ -328,17 +329,17 @@ async function run() {
 
     // // get data by id
 
-    app.get('/featured/jobs/:id',async (req,res) => {
+    app.get('/featured/jobs/:id', async (req, res) => {
       try {
-        let id=req.params.id
+        let id = req.params.id
         console.log(id);
-        
-        let result=await featuredcompanyJobsCollection.findOne({_id:new ObjectId(id)})
+
+        let result = await featuredcompanyJobsCollection.findOne({ _id: new ObjectId(id) })
         console.log(result);
-        
+
         res.send(result)
       } catch (error) {
-        res.send({message:error.message})
+        res.send({ message: error.message })
       }
     })
 
@@ -414,9 +415,9 @@ async function run() {
         // console.log(companyJobs);
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 5;
-        const companyName=req.query.companyName
-      
-        
+        const companyName = req.query.companyName
+
+
 
         let isResult = await companyJobsCollection.deleteMany()
 
